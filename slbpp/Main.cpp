f4234d69d506f2bbbbfe377c7203220239ba12e9
Szeba25 <lustmord20@gmail.com>
2019-10-06 14:04:56 +0200

    Added a simple render loop

--- slbpp/Main.cpp
+++ slbpp/Main.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
 int main() {
 	
 	// Initialize GLFW with OpenGL Version 3.3 and core profile.
@@ -11,7 +15,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "slb++", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "slbpp", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -24,7 +28,14 @@ int main() {
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1280, 720);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
